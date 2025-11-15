ec5216e1116f0854446c391ac7ff6f9800b17d77
NesrineCbn <nesrinechahboune21@gmail.com>
2025-11-15 01:13:53 +0100

    Init portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from './utils/Themes.js'
+import Navbar from "./componants/Navbar";
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import HeroSection from "./componants/HeroSection";
+import About from "./componants/About";
+import Skills from "./componants/Skills";
+import Projects from "./componants/Projects";
+import Contact from "./componants/Contact";
+import Footer from "./componants/Footer";
+import Experience from "./componants/Experience";
+import Education from "./componants/Education";
+import ProjectDetails from "./componants/ProjectDetails";
+import styled from "styled-components";
 
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 function App() {
+  const [darkMode, setDarkMode] = useState(false); // mettre false pour le thème clair
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}> {/* forcer le thème clair */}
+      <Router>
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+            <Contact />
+          </Wrapper>
+          <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
+        </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App;  
