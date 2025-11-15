34a881e3d67a7da75453ed0044c51932aa55a890
NesrineCbn <nesrinechahboune21@gmail.com>
2025-11-15 02:01:02 +0100

    Initial commit portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,264 @@
+export const Bio = {
+  name: "Nesrine",
+  roles: [
+    "Étudiante en Réseaux",
+  ],
+  description:
+    "Intéressée par le cloud et les infrastructures réseau, j’aime comprendre comment les technologies connectent le monde et comment les sécuriser.",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+
+};
+
+export const skills = [
+  {
+    title: "Cloud & Automatisation",
+    skills: [
+      {
+        name: "Ansible",
+        image:
+          "ansible.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "Docker.png",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "Kuber.png",
+      },
+      {
+        name: "Packer",
+        image: "packer.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "aws.png",
+      },
+      {
+        name: "Huawei Cloud Stack",
+        image:
+          "huaw.png",
+      },
+      {
+        name: "Openstack",
+        image:
+          "openstak.png",
+      },
+    ],
+  },
+  {
+    title: "Langages & Développement",
+    skills: [
+
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "C",
+        image: "c.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+       {
+        name: "React Js",
+        image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+    ],
+  },
+  {
+    title: "Système",
+    skills: [
+      {
+        name: "Ubuntu",
+        image:
+          "ubuntu.png",
+      },
+      {
+        name: "Debian",
+        image:
+          "debian.png",
+      },
+      {
+        name: "Kali",
+        image:
+          "kali.png",
+      },
+      {
+        name: "Windows",
+        image:
+          "windows.png",
+      },
+    ],
+  },
+  {
+    title: "Autres",
+    skills: [
+      {
+        name: "Wireshark",
+        image:
+          "wireshark.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Vmware",
+        image:
+          "vmware.png",
+      },
+      {
+        name: "VirtualBox",
+        image:
+          "virtualbox.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+          {
+        name: "Ns3",
+        image:
+          "ns3.png"
+      },
+      {
+        name: "Cisco Packet Tracer",
+        image:
+          "cisco.png",
+      },
+      {
+        name: "Omnet++",
+        image:
+          "omnet.png"
+        },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "LogoDJEZZY.png",
+    role: "Stagiaire Ingénieure Solution Cloud",
+    company: "Djezzy - Alger",
+    date: "janvier 2024 - Juin 2024",
+    desc: "Durant mon stage, j'ai automatisé le déploiement et la configuration de Nextcloud sur Huawei Cloud Stack. J'ai conçu l'architecture du service en tenant compte des besoins de sécurité, performance et isolation des données. J'ai créé des images serveur préconfigurées avec Packer et Cloud-init, permettant un déploiement rapide et cohérent des instances. L'approvisionnement de l'infrastructure a été automatisé avec Service Builder, et la configuration initiale des serveurs a été réalisée via user-data. Enfin, j'ai utilisé Ansible pour gérer de manière organisée et répétable la configuration complète des serveurs, assurant une installation fiable et facilement maintenable."
+
+  },
+  {
+    id: 0,
+    img: "LogoMOBILIS.png",
+    role: "Stagiaire Administratrice ",
+    company: "Mobilis",
+    date: "Juin 2023 - Aout 2023",
+    desc: "Lors d'un stage pratique en administration système, j'ai acquis une expérience approfondie dans la gestion et la configuration des systèmes et réseaux informatiques. J'ai travaillé sur l'installation et l'administration de stations de travail et de serveurs Linux, la configuration de réseaux avec DHCP, DNS, LDAP, NFS et services web, ainsi que la mise en place de serveurs de messagerie Postfix, tout en appliquant des procédures de sauvegarde et de sécurité.",
+  },
+  {
+    id: 1,
+    img: "LogoMOBILIS.png",
+    role: "Stagiaire Ingénieure Transmission",
+    company: "Mobilis",
+    date: "Mars 2022 - Juin 2022",
+    desc: "Pendant mon stage chez Mobilis à Alger, j’ai travaillé sur la transmission par micro-ondes afin de répondre à l’augmentation des capacités de transmission dans les réseaux mobiles, notamment avec l’arrivée de la 5G. J’ai étudié les différentes générations de réseaux, les supports de transmission, et me suis concentrée sur les faisceaux hertziens. J’ai participé à la conception d’une solution utilisant la bande de fréquence E-band, en configurant et mettant à jour les liaisons via le logiciel Ellipse, ce qui a permis d’atteindre plusieurs gigabits par seconde.",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "LogoUSPN.png",
+    school: "Master Réseaux",
+    date: "2024 - 2026",
+    desc: "Ce parcours me forme aux technologies actuelles et émergentes, telles que les communications mobiles, la virtualisation, la sécurité réseau et l’Internet des objets, consolidant mes compétences pour des projets professionnels et des stages qualifiants.",
+    degree: "Université Sorbonne Paris Nord",
+  },
+  {
+    id: 1,
+    img: "LogoUSTHB.png",
+    school: "Master Réseaux et Systèmes Distribués",
+    date: "2022 - 2024",
+    desc: " Ce master m’a spécialisé dans les systèmes distribués et l’administration de réseaux d’entreprise, en approfondissant mes compétences en sécurité, optimisation, applications réparties et gestion de projets informatiques.",
+    degree: "Université des Sciences et de la Technologie Houari Boumediene (Alger)",
+  },
+  {
+    id: 2,
+    img: "LogoUSTHB.png",
+    school: "Licence Génie des Télécommunications et Réseaux",
+    date: "2019 - 2022",
+    desc: "Cette formation m’a permis de développer un double profil en électronique et informatique, capable de concevoir, installer et administrer des réseaux et systèmes de télécommunications.",
+    degree: "Université des Sciences et de la Technologie Houari Boumediene (Alger)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Conception et mise en œuvre d’un réseau d’entreprise sécurisé",
+    description:
+      "Conception et déploiement d’un réseau d’entreprise sécurisé, évolutif et performant pour une société de développement logiciel de taille moyenne. Le projet comprend la segmentation par VLAN, le routage OSPF, la configuration d’un pare-feu et de listes de contrôle d’accès (ACL), la mise en place d’un VPN pour les employés à distance et d’un Wi-Fi d’entreprise pour le personnel sur site. L’ensemble garantit la confidentialité, l’intégrité et la disponibilité des données tout en respectant les bonnes pratiques Cisco.",
+    image:
+      "sdtcn.png",
+    tags: [
+      "Firewall",
+      "OSPF",
+      "VLAN",
+      "NAT",
+      "Cisco ASA",
+    ],
+  },
+  {
+    id: 0,
+    title: "Pipeline CI/CD Complet avec Jenkins, Docker et Kubernetes",
+    description:
+      "Mise en place d’un pipeline CI/CD complet permettant le déploiement automatisé d’une application sur un cluster Kubernetes à l’aide de Jenkins. Le pipeline intègre le code depuis Git, génère et pousse des images Docker vers un registre, exécute des playbooks Ansible, puis déploie automatiquement les conteneurs sur Kubernetes.",
+    image:
+      "Capture.PNG",
+    tags: ["CI/CD Pipeline", "Git", "Kubernetes", "Jenkins", "Docker"],
+  },
+  {
+    id: 1,
+    title: "Plateforme Mail Exchange 2016 sous Active Directory",
+    description:
+      "Mise en place d’une infrastructure de messagerie avec Exchange Server 2016, intégrée à Active Directory sur Windows Server. Configuration du domaine, des comptes utilisateurs, des boîtes aux lettres, des connecteurs mail, assurant une plateforme de messagerie interne fonctionnelle et sécurisée.",
+    image:
+      "ex.PNG",
+    tags: [
+      "Exchange Server 2016",
+      "Active Directory",
+      "Serveur de messagerie",
+      "Contrôleur de domaine",
+    ],
+  },
+];
+
